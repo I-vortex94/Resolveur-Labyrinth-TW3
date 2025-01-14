260efbd91895c389acfe739c43802534429dd29a
I-vortex94 <131229765+I-vortex94@users.noreply.github.com>
2025-01-14 22:25:07 +0100

    Constantes

---
+++ constants.py
@@ -0,0 +1,8 @@
+from typing import Tuple
+
+# Coord est un alias pour un tuple de deux entiers représentant une coordonnée (x, y)
+Coord = Tuple[int, int]
+
+# Constantes pour la taille du labyrinthe
+DEFAULT_WIDTH = 31
+DEFAULT_HEIGHT = 41
