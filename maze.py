5c845742c1740b1ad78a4ba1b3110576687648d1
I-vortex94 <131229765+I-vortex94@users.noreply.github.com>
2025-01-14 22:23:58 +0100

    Booléen

--- maze.py
+++ maze.py
@@ -1,5 +1,6 @@
-import random 
+import random
 from typing import List, Tuple
+from constants import Coord
 
 class MazeGenerator:
     """
@@ -15,26 +16,29 @@ class MazeGenerator:
             height += 1
         self.width: int = width #longeur du laby
         self.height: int = height #hauteur du laby
-        self.maze: List[List[str]] = [['#' for _ in range(width)] for _ in range(height)]
 
-    def generate(self) -> None: #Pour faire le laby
+    def generate(self) -> Tuple[List[List[bool]]], Tuple[Coord, Coord]: #Pour faire le laby
         """
         Placer le départ (D)
         """
+        # Créer une matrice de murs (False) et de passages (True)
+        maze: List[List[bool]] = [[False for _ in range(self.width)] for _ in range(self.height)]
+
         start_x: int = random.randrange(1, self.width - 1, 2) # 2 pour s'assurer qu'il ne tombe pas sur un bord
         start_y: int = random.randrange(1, self.height - 1, 2) # 2 pour s'assurer qu'il ne tombe pas sur un bord
-        self.maze[start_y][start_x] = 'D' #Place le départ (D) sur le laby
+        self.maze[start_y][start_x] = True #Place le départ sur le laby : True
+    
+        #On défini les directions possibles pour les neighbors (haut, bas, gauche, droite)
+        directions: List[Coord] = [(-2, 0), (2, 0), (0, -2), (0, 2)]
 
-        to_explore: List[Tuple[int, int]] = [(start_x, start_y)] # Initialisation (nan c pas une récurrence...) pour générer un laby faisable
+        to_explore: List[Coord] = [(start_x, start_y)] # Initialisation (nan c pas une récurrence...) pour générer un laby faisable
 
         """
         Générer le laby
         """
         while to_explore:
             x, y = to_explore[-1] #Prend la dernière cellule
-            neighbors: List[Tuple[int, int]] = [] #On va lister les neighbors de cette cellule dans... une liste
-            #On défini les directions possibles pour les neighbors (haut, bas, gauche, droite)
-            directions: List[Tuple[int]] = [(-2, 0), (2, 0), (0, -2), (0, 2)]
+            neighbors: List[Coord] = [] #On va lister les neighbors de cette cellule dans... une liste
 
             for dx, dy in directions:
                 nx, ny = x + dx, y + dy
@@ -70,12 +74,12 @@ class MazeGenerator:
             end_y = random.randrange(1, self.height, 2)
         self.maze[end_y][end_x] = 'A' #Marqué l'arrivée
 
-    def display(self):
+    def display(self, maze: List[List[bool]]) -> None:
         """
         Affiche le laby
         """
-        for ligne in self.maze:
-          print(''.join(ligne))
+        for row in maze:
+          print(''.join(['#' if not cell esle ' ' for cell in row))
 
 width: int = 31 #Mettre des nombres impaires
 height: int = 41 #Mettre des nombres impaires
